be5dd131c55c25480687b55247ef96a1d679be85
Sunset <sunset@kali.box>
2022-04-25 21:25:11 -1000

    First Exercice

---
+++ JS/EXERCICIOS/sistemaDeNotasEscolares.js
@@ -0,0 +1,22 @@
+let nota = 72
+
+switch (nota) {
+    case (nota >= 90):
+        console.log('A')
+        break
+    case (nota >= 80):
+        console.log('B')
+        break
+    case (nota >= 70):
+        console.log('C')
+        break
+    case (nota >= 60):
+        console.log('D')
+        break
+    case (nota >= 50):
+        console.log('E')
+        break
+    case (nota < 50):
+        console.log('F')
+        break
+} 
