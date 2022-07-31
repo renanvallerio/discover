c23328a600f4cebd6ca805aa3d7a27e51ba433ac
renanvallerio <renanvallerio@gmail.com>
2022-07-30 17:15:29 -1000

    ForEach

--- NodeJS+EJS/server.js
+++ NodeJS+EJS/server.js
@@ -4,7 +4,43 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('pages/index')
+  const items = [
+    {
+      title: "A",
+      message: " mazing develop applications/sevices easily"
+    },
+    {
+      title: "W",
+      message: " onderful"
+    },
+    {
+      title: "E",
+      message: " JS uses JavaScript to render HTML"
+    },
+    {
+      title: "S",
+      message: " imple Sintax"
+    },
+    {
+      title: "O",
+      message: " ptimized"
+    },
+    {
+      title: "M",
+      message: " ulti-use"
+    },
+    {
+      title: "E",
+      message: " xtraordinary"
+    },
+  ];
+
+  const subtitle = "It's a modeling language for creating HTML pages using JS"
+
+  res.render('pages/index', {
+    qualities: items,
+    subtitle: subtitle,
+  })
 })
 
 app.get('/about', function (req, res) {
